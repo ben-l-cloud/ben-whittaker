6b62d9651af1c125274b9aa1a608e31b06dadd1b
ben-l-cloud <whittakerben70@gmail.com>
2025-06-10 22:48:33 +0300

    Delete commands/kiss.js

--- commands/kiss.js
+++
@@ -1,9 +0,0 @@
-{
-  name: "kiss",
-  description: "😘 Send a random kiss GIF",
-  async execute(sock, msg) {
-    const res = await axios.get(`https://tenor.googleapis.com/v2/search?q=kiss&key=AIzaSyCIDY_QAXl2ZKdJt45aakaTINvJY_YpefM&limit=1`);
-    const gifUrl = res.data.results[0]?.media_formats?.gif?.url;
-    await sock.sendMessage(msg.key.remoteJid, { video: { url: gifUrl }, gifPlayback: true, caption: "😘 Mwah!" });
-  }
-}
